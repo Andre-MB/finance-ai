01d822091a40bca50eb9d1beee589979174c0814
Andre-MB <andrezinhogame15@gmail.com>
2024-11-11 20:18:59 -0300

    chore: Title and button

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,26 @@
-const Transaction = () => {
-  return <>Page</>;
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+
+const Transactions = async () => {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div>
+      {transactions.map((transactions) => (
+        <div key={transactions.id}> {transactions.name}</div>
+      ))}
+
+      {/* TÍTULO E BOTÃO */}
+      <div className="flex w-full items-center justify-between p-6">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="roundd-full">
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
-export default Transaction;
+export default Transactions;
