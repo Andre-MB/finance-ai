2c82506612eb5e0afc4f9c72382eed264ee2584a
Andre-MB <andrezinhogame15@gmail.com>
2024-11-14 20:25:31 -0300

    chore: add table and badge the shadcn

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,24 +1,28 @@
 import { ArrowDownUpIcon } from "lucide-react";
 import { Button } from "../_components/ui/button";
+import { DataTable } from "../_components/ui/data-tablel";
 import { db } from "../_lib/prisma";
+import { transactionColumns } from "./_columns";
 
 const Transactions = async () => {
   const transactions = await db.transaction.findMany({});
 
   return (
-    <div>
-      {transactions.map((transactions) => (
+    <div className="space-y-6 p-6">
+      {/* {transactions.map((transactions) => (
         <div key={transactions.id}> {transactions.name}</div>
-      ))}
+      ))} */}
 
       {/* TÍTULO E BOTÃO */}
-      <div className="flex w-full items-center justify-between p-6">
+      <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="roundd-full">
+        <Button className="rounded-full">
           Adicionar transação
           <ArrowDownUpIcon />
         </Button>
       </div>
+
+      <DataTable columns={transactionColumns} data={transactions} />
     </div>
   );
 };
