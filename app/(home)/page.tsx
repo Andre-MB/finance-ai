66f62132ec4261e43a45c586fc5ea986209d629e
Andre-MB <andrezinhogame15@gmail.com>
2025-10-02 03:15:55 -0300

    feat: add transactions pie chart component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,58 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Navbar from "../_components/navar";
+import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time-select";
+import { isMatch } from "date-fns";
+import TransactionsPieChart from "./_components/transactions-pie-char";
+import { getDashboard } from "../_data/get-dashbiard";
+
+interface HomeProps {
+  searchParams: {
+    month: string;
+  };
+}
+
+const Home = async ({ searchParams: { month } }: HomeProps) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const monthIsInValid = !month || !isMatch(month, "MM");
+
+  if (monthIsInValid) {
+    redirect("?month=01"); // Default to January if no valid month is provided
+  }
+
+  const dashboard = await getDashboard(month);
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="flex flex-col gap-6">
+            {/* <div className="grid grid-cols-[2fr,1fr]"> */}
+            <SummaryCards month={month} {...dashboard} />
+
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionsPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
+
+        {/*  */}
+      </div>
+    </>
+  );
+};
+
+export default Home;
